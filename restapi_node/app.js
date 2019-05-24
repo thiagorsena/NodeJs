d0a60082004ae221c4cf946cde3b45c44dd33cf0
Thiago R Sena <thiagorsena@gmail.com>
2019-05-23 23:18:49 -0300

    rest api Node

---
+++ restapi_node/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var produtosRouter = require('./routes/produtosRouter');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+var url = 'mongodb://thiagorsena:thiago123@ds018268.mlab.com:18268/produtosdb'
+
+var db = mongoose.connect(url);
+
+var app = express();
+app.use(bodyParser.json());
+
+app.listen(5000, function () {
+    console.log('Servidor escutando na porta 5000...');
+});
+
+app.use('/produtos',produtosRouter);       
